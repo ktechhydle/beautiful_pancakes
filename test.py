c7a7c04cad15e00fd7618f5a70d7a39175dfcf72
kelle <kellerhydle@gmail.com>
2024-08-23 14:30:28 -0600

    #5 minor updates
    -added test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from managers import BeautifulPancakeCreator
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene)
+
+        self.setCentralWidget(self.view)
+        self.setWindowTitle("Demo")
+
+        # Add items to the scene
+        rect_item = QGraphicsRectItem(0, 0, 100, 100)
+        self.scene.addItem(rect_item)
+
+        manager = BeautifulPancakeCreator(self.scene, self, app_name='Demo', app_extension='.demo')
+        manager.save()
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
